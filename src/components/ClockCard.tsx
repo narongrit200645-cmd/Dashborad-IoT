e3f5355c8dce8ab19523f2f82e72daff10bd3888
narongrit200645-cmd <narongrit200645@gmail.com>
2026-07-29 17:23:29 +0700

    Update ClockCard.tsx

--- src/components/ClockCard.tsx
+++ src/components/ClockCard.tsx
@@ -82,7 +82,7 @@ export const ClockCard: React.FC<ClockCardProps> = ({
         }`}
       >
         <img
-          src={clockLogoUrl || "/src/public/Logo NA"} // เรียกใช้รูปภาพจากโฟลเดอร์ public
+          src={clockLogoUrl || "Logo NA.png"} // เรียกใช้รูปภาพจากโฟลเดอร์ public
           alt="Logo NA"
           className="w-full h-full max-h-24 object-contain rounded-md"
         />
